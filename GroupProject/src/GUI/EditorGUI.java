4a6e3b5b59401a35ce5d4aa1b36030dfda6dc9db
NicholasJKlein <njklein@gmail.com>
2017-02-17 10:06:00 -0500

    Starting user interface for editor gui

---
+++ GroupProject/src/GUI/EditorGUI.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
+
+public class EditorGUI {
+	
+	JFrame frame;
+	JPanel panel;
+	JScrollBar list;
+	JComboBox<Integer> levels;
+	JButton record;
+	JButton replay;
+	JButton saveWord;
+	JButton deleteWord;
+	JButton saveLevel;
+	JButton deleteLevel;
+	JTextField takeWord;
+	JTextField newLevelNum;
+	JLabel title;
+	JLabel timer;
+	
+	
+
+}
