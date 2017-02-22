d6968e703a4c16fff518d8397a864335ce8e7bc2
lcary1992 <lcary1992@CHHB45C4279495.inst.hcpss.org>
2017-02-22 10:09:24 -0500

    added methods to BackEnd class

--- GroupProject/src/BackEnd/BackEnd.java
+++ GroupProject/src/BackEnd/BackEnd.java
@@ -6,9 +6,15 @@ public class BackEnd {
 	private ArrayList<ArrayList<Word>> levels = new ArrayList<ArrayList<Word>>();
 	
 	BackEnd(){
-		//Loads all Words int levels
+
+		
 		
 	}
+	//adds desired word
+	public void addWord(Word w, int level) {
+		levels.get(level-1).add(w);
+	}
+	//removes desired word
 	public void removeWord(Word w,int level){
 		for(int i = 0;i<levels.get(level-1).size();i++){
 			if(w.getSpelling().equals(levels.get(level-1).get(i).getSpelling())){
@@ -16,4 +22,42 @@ public class BackEnd {
 			}
 		}
 	}
+	//adds highest level
+	public void addLevel() {
+		levels.add(new ArrayList<Word>());
+	}
+	//adds desired level
+	public void addLevel(int level) {
+		levels.add(level-1,new ArrayList<Word>());
+	}
+	//removes desired level
+	public void removeLevel(int level) {
+		levels.remove(level-1);
+	}
+	//returns next word
+	public Word nextWord(Word word) {
+		
+		int c = word.getLevel()-1;
+		int index = 0;
+		
+		for(int i = 0; i < levels.get(c).size();i++) {
+			
+			if(levels.get(c).get(i).getSpelling().equals(word.getSpelling())) {
+				
+				if(i+1==levels.get(c).size()) {
+					if(c == levels.size()-1) 
+						return null;
+					c++;
+					index = 0;
+					return levels.get(c).get(index);
+				} else {
+					index = i+1;
+					return levels.get(c).get(index);
+				}
+				
+			}	
+		}
+		return null;
+	}
+	
 }
