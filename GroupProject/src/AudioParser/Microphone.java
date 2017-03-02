b83c9260ad095a60560606fbb014c2d606817b87
jwang7517 <jwang7517@CHHB45C4278DE1.inst.hcpss.org>
2017-03-02 09:50:24 -0500

    audio

--- GroupProject/src/AudioParser/Microphone.java
+++ GroupProject/src/AudioParser/Microphone.java
@@ -80,7 +80,7 @@ public class Microphone {
      
     public static void main(String[] args) {
 		Microphone tse = new Microphone();
-    	tse.fileReceive("six");
+    	tse.fileReceive("tube");
     }
  
 }
