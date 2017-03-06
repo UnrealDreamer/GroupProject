39616c07f74c4a6749bbddc6506c97a9479db53a
jwang7517 <jwang7517@CHHB45C4278DE1.inst.hcpss.org>
2017-03-06 10:08:58 -0500

    audio

--- GroupProject/src/AudioParser/Microphone.java
+++ GroupProject/src/AudioParser/Microphone.java
@@ -80,7 +80,7 @@ public class Microphone {
      
     public static void main(String[] args) {
 		Microphone tse = new Microphone();
-    	tse.fileReceive("melody");
+    	tse.fileReceive("embezzle");
     }
  
 }
