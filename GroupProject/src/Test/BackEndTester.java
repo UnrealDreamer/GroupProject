6ac18439c466581ebde8be449813aa6b143f73d5
lcary1992 <lcary1992@CHHB45C4279495.inst.hcpss.org>
2017-03-07 10:00:43 -0500

    BackEnd can change a word's spelling or remove words (does so for all existing users as well)
    BackEndTester tests new methods

--- GroupProject/src/Test/BackEndTester.java
+++ GroupProject/src/Test/BackEndTester.java
@@ -9,13 +9,10 @@ public class BackEndTester {
 	public static void main(String[] args) {
 		User testUser = new User("Joe",10);
 		BackEnd test = new BackEnd(testUser);
-		//testUser.addWord(new Word("character",3));
-		//testUser.addWord(new Word("porcupine",3));
-		//testUser.addWord(new Word("flamingo",3));
+		
+		
+		
 		test.exit();
-		test.printAllWords();
-		System.out.println("____________________________");
-		test.printUserWords();
 	}
 
 }
