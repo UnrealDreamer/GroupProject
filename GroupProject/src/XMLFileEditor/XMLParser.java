11667e6b09f86359854a025eb8cbe512eae8ebd2
lcary1992 <lcary1992@CHHB45C4279495.inst.hcpss.org>
2017-03-30 09:29:00 -0400

    bug fixes

--- GroupProject/src/XMLFileEditor/XMLParser.java
+++ GroupProject/src/XMLFileEditor/XMLParser.java
@@ -139,10 +139,11 @@ public class XMLParser {
 				construct.write("\n\t\t<" + elements[2] + '>');
 				for(int i = 0; i < user[ind].getCorrectlySpelt().size(); i++)
 				{
-					if(user[ind].getCorrectlySpelt().get(i)!=null)
+					if(user[ind].getCorrectlySpelt().get(i)!=null) {
 						construct.write(user[ind].getCorrectlySpelt().get(i).getSpelling());
 					if(i < user[ind].getCorrectlySpelt().size() - 1)
 						construct.write(" ");
+					}
 				}
 				construct.write( "</" + elements[2] + '>');
 				construct.write("\n\t</User>");
